52a7b7007c70c456b1ec96e53cbdf05784e986e8
Erfan Asadi <asadi.love7981@gmail.com>
2022-12-16 14:34:59 +0330

    routing - end s4

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import React from 'react'
-import Form from './components/Form';
-import Nav from './components/Nav'
-import NotesList from './components/NotesList'
-import NotesProvider from './contexts/NotesContext';
+import React from 'react';
+import Home from './pages/Home';
+import About from './pages/About';
+import Product from './pages/Product';
+import Nav from './Nav';
 
-
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
     <div>
-      <NotesProvider>
-          <Nav />
-          <Form />
-          <NotesList />
-      </NotesProvider>
+      <BrowserRouter>
+      <Nav />
+        <Routes>
+          <Route path='/' element={ <Home />} />
+          <Route path='/product' element={ <Product />} />
+          <Route path='/about' element={ <About />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
