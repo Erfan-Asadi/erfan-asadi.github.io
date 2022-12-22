d8f46ec9585af3eeddc261e2295e0186834164a3
Erfan Asadi <asadi.love7981@gmail.com>
2022-12-22 12:49:57 +0330

    init

--- src/App.js
+++ src/App.js
@@ -1,57 +1,9 @@
-import React, { createRef, useRef, useState } from "react";
-import './App.css';
-import InputField from "./Components/InputField";
+import React from 'react'
 
-function App() {
-  const inputRefs = useRef([
-    createRef(), createRef()
-  ]);
-  const [data, setData] = useState({});
-
-  function handleChange(name , value) {
-    setData(prev => ({...prev, [name]: value}))
-  }
-
-  function submitForm(e) {
-    e.preventDefault();
-    let isValid = true;
-
-    for(let i=0; i < inputRefs.current.length; i++) {
-      const valid = inputRefs.current[i].current.validate();
-      if(!valid) {
-        isValid = false;
-      }
-    }
-    if(!isValid) {
-      return
-    }
-    console.log("Logged In!")
-    // Carry on as normal
-  }
-  console.log(data)
+const App = () => {
   return (
-    <div className="App">
-      <form onSubmit={submitForm} className="form">
-        <h1>React Register Form</h1>
-        <InputField
-          ref={inputRefs.current[0]}
-          name='username'
-          label='username*:'
-          value={data.username}
-          onChange={handleChange}
-          validation="required|min:6|max:12"  />
-        <InputField
-          ref={inputRefs.current[1]}
-          value={data.password}
-          name='password'
-          label='password*:'
-          type='password'
-          onChange={handleChange}
-          validation="required|min:6"  />
-          <button>Login</button>
-      </form>
-    </div>
+    <div>App</div>
   )
 }
 
-export default App;
+export default App
